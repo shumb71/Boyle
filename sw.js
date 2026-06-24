1aa942872f46406c515e96f2965c87da8b0dbfe0
shumb71 <shumb71@gmail.com>
2026-06-24 20:26:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,13 @@
-const CACHE = 'boyle-v2-102';
+const CACHE = 'boyle-v2-103';
 const ASSETS = ['./'];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
 self.addEventListener('fetch', e => {
